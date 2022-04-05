bdfa8a53985807635ca74831aa46eb292088b36a
Manish Goud Bandharapu <89554539+919610362@users.noreply.github.com>
2022-04-05 10:48:40 -0500

    Add files via upload

---
+++ Bandharapu_GroceryApp/Bandharapu_GroceryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bandharapu_GroceryApp
+//
+//  Created by Bandharapu,Manish Goud on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bandharapu_GroceryApp/GrocerySections.swift
@@ -0,0 +1,13 @@
+//
+//  GrocerySections.swift
+//  Bandharapu_GroceryApp
+//
+//  Created by Bandharapu,Manish Goud on 4/5/22.
+//
+
+import Foundation
+
+struct Grocery {
+    var section : String;
+    var items_array 
+}
